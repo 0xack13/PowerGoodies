f068d9d68370c94ea6537efe9f3e9d1277f97367
0xack13 <0xack13@gmail.com>
2014-08-19 12:07:50 +0300

    Initial commit

---
+++ Get-FolderSize.ps1
@@ -0,0 +1,21 @@
+﻿
+Function Get-FolderSize
+
+{
+
+ BEGIN{$fso = New-Object -comobject Scripting.FileSystemObject}
+
+ PROCESS{
+
+    $path = $input.fullname
+
+    $folder = $fso.GetFolder($path)
+
+    $size = $folder.size
+
+    [PSCustomObject]@{'Name' = $path;'Size (GB)' = ($size / 1gb) 
+    } 
+   }
+ }
+
+ Get-ChildItem D:\DevCode\0xack13 | Get-FolderSize
