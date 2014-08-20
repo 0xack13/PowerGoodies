b4a33380c135912242c420c34329c6e8696fe2f5
0xack13 <0xack13@gmail.com>
2014-08-20 15:42:29 +0300

    Get Wireless Autoprofile

---
+++ Get-WirelessAutoProfile.ps1
@@ -0,0 +1,23 @@
+﻿function Get-WirelessAutoProfile
+{
+$GUID = (Get-NetAdapter -Name 'wi-fi').interfaceGUID
+
+$path = "C:\ProgramData\Microsoft\Wlansvc\Profiles\Interfaces\$guid"
+
+Get-ChildItem -Path $path -Recurse |
+
+Foreach-Object {
+
+   [xml]$c = Get-Content -path $_.fullname
+
+   New-Object pscustomobject -Property @{
+
+   'name' = $c.WLANProfile.name;
+
+   'mode' = $c.WLANProfile.connectionMode;
+
+   'ssid' = $c.WLANProfile.SSIDConfig.SSID.hex} 
+   }
+ }
+
+ Get-WirelessAutoProfile
